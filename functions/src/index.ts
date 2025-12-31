af8f0741388e368c4fa26dab67e75570320f8d5e
iChanceTek <dev@ichancetek.com>
2025-12-31 18:42:17 -0500

    Updated index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,17 @@
 /**
- * Firebase Functions (Gen 2)
+ * Firebase Functions entry point
  * https://firebase.google.com/docs/functions
  */
 
-import { setGlobalOptions } from "firebase-functions/v2";
-import { onRequest } from "firebase-functions/v2/https";
+import { setGlobalOptions } from "firebase-functions";
+import { onRequest } from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
 
-// Apply global options to all Gen 2 functions
+// Apply global options to all functions
 setGlobalOptions({ maxInstances: 10 });
 
-// Example HTTP function
+// Example HTTP function (required so imports are used)
 export const helloWorld = onRequest((req, res) => {
     logger.info("Hello logs!", { structuredData: true });
-    res.send("Hello from Firebase!");
+    res.status(200).send("Hello from Firebase!");
 });
