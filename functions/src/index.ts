cc32320dd7d770592595ed8ceb46e9b3e30cc793
iChanceTek <dev@ichancetek.com>
2025-12-31 19:34:34 -0500

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,4 @@
 import { setGlobalOptions } from "firebase-functions";
-import { onRequest } from "firebase-functions/https";
 
 // Apply global options to all functions
 setGlobalOptions({ maxInstances: 10 });
-
-// Simple HTTP test function
-export const helloWorld = onRequest((req, res) => {
-    res.status(200).send("Hello from Firebase!");
-});
